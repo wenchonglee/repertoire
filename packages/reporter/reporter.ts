14d130dc95d544a05aa3e45116d2d39b933fdd1b
wenchong <18256786+wenchonglee@users.noreply.github.com>
2023-05-30 23:34:27 +0800

    updated reporter to reuse context

--- packages/reporter/reporter.ts
+++ packages/reporter/reporter.ts
@@ -1,15 +1,12 @@
-import { request } from "@playwright/test";
+import { APIRequestContext, request } from "@playwright/test";
 import { FullConfig, FullResult, Reporter, Suite, TestCase, TestResult } from "@playwright/test/reporter";
 import { simpleHash } from "./simpleHash";
 
 const RUN_ID = simpleHash(new Date().toISOString());
 
-const reportTestRunStart = async (suite: Suite) => {
-  const context = await request.newContext({
-    baseURL: "http://localhost:3000/api/",
-  });
+const reportTestRunStart = async (suite: Suite, context?: APIRequestContext) => {
   try {
-    const response = await context.post("./runs", {
+    const response = await context?.post("./runs", {
       data: {
         runId: RUN_ID,
         startTime: new Date().toISOString(),
@@ -25,12 +22,9 @@ const reportTestRunStart = async (suite: Suite) => {
   }
 };
 
-const reportTestRunEnd = async () => {
-  const context = await request.newContext({
-    baseURL: "http://localhost:3000/api/",
-  });
+const reportTestRunEnd = async (context?: APIRequestContext) => {
   try {
-    await context.put(`./runs/${RUN_ID}`, {
+    const response = await context?.put(`./runs/${RUN_ID}`, {
       data: {
         endTime: new Date().toISOString(),
       },
@@ -41,78 +35,63 @@ const reportTestRunEnd = async () => {
   }
 };
 
-const reportTestStart = async (test: TestCase, result: TestResult) => {
-  const context = await request.newContext({
-    baseURL: "http://localhost:3000/api/",
-  });
+const reportTestStart = async (test: TestCase, result: TestResult, context?: APIRequestContext) => {
   try {
-    const response = await context.post(`./runs/${RUN_ID}/tests`, {
+    const response = await context?.post(`./runs/${RUN_ID}/tests`, {
       data: {
         testId: test.id,
         title: test.title,
         startTime: new Date().toISOString(),
       },
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: { "content-type": "application/json" },
     });
-    console.log(response.url());
   } catch (err) {
     console.log(err);
   }
 };
 
-const reportTestEnd = async (test: TestCase, result: TestResult) => {
-  const context = await request.newContext({
-    baseURL: "http://localhost:3000/api/",
-  });
-  console.log("1");
+const reportTestEnd = async (test: TestCase, result: TestResult, context?: APIRequestContext) => {
   try {
-    const response = await context.put(`./runs/${RUN_ID}/tests/${test.id}`, {
+    const response = await context?.put(`./runs/${RUN_ID}/tests/${test.id}`, {
       data: {
         endTime: new Date().toISOString(),
         outcome: test.outcome,
         errors: JSON.stringify(result.errors),
       },
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: { "content-type": "application/json" },
     });
-    console.log(response);
-    console.log(response.url());
   } catch (err) {
-    console.log("3");
     console.log(err);
   }
 };
 
 class MyReporter implements Reporter {
-  onBegin(config: FullConfig, suite: Suite) {
+  context: APIRequestContext | undefined;
+  baseURL: "http://localhost:3000/api/";
+
+  async onBegin(config: FullConfig, suite: Suite) {
     console.log(`Starting the run with ${suite.allTests().length} tests`);
     // console.log(Object.keys(process.env));
-    // console.log(suite.allTests().map((test, index) => `Test ${index} - ID: ${test.id}, Title: ${test.title}`));
-    reportTestRunStart(suite);
+
+    this.context = await request.newContext({ baseURL: this.baseURL });
+    reportTestRunStart(suite, this.context);
   }
 
   onTestBegin(test: TestCase, result: TestResult) {
     console.log(`Starting test ID: ${test.id}, Title: ${test.title}`);
-    // send start time
-    reportTestStart(test, result);
+
+    reportTestStart(test, result, this.context);
   }
 
   onTestEnd(test: TestCase, result: TestResult) {
     console.log(`Finished test ${test.title}: ${result.status}`);
-    // console.log(JSON.stringify(result, getCircularReplacer()));
-    // send end time
-    reportTestEnd(test, result);
-    // console.log(test);
 
-    console.log(result);
+    reportTestEnd(test, result, this.context);
   }
 
   async onEnd(result: FullResult) {
     console.log(`Finished the run: ${result.status}`);
-    await reportTestRunEnd();
+    await reportTestRunEnd(this.context);
   }
 }
 
