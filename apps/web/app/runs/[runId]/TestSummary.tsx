26516209b3a85bc89067a859e2ad1d8c772c68ca
wenchong <18256786+wenchonglee@users.noreply.github.com>
2023-06-03 23:10:07 +0800

    changed tests to be cards

--- apps/web/app/runs/[runId]/TestSummary.tsx
+++ apps/web/app/runs/[runId]/TestSummary.tsx
@@ -1,8 +1,9 @@
 import type { TestResponse } from "@/app/api/runs/[runId]/tests/route";
-import { Table } from "@/components/Table";
+import { Card } from "@/components/Card";
 import dayjs from "dayjs";
+import { AlertCircle, CheckCircle2, SkipForward, TimerOff, XCircle } from "lucide-react";
 
-const getData = async (runId: string): Promise<TestResponse[]> => {
+const getData = async (runId: string): Promise<{ _id: string; tests: TestResponse[] }[]> => {
   const res = await fetch(`http://localhost:3000/api/runs/${runId}/tests`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -16,28 +17,59 @@ export default async function TestSummary(props: { runId: string }) {
   const data = await getData(props.runId);
 
   return (
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.Head>Test ID</Table.Head>
-          <Table.Head>Title</Table.Head>
-          <Table.Head>Duration</Table.Head>
-        </Table.Row>
-      </Table.Header>
+    <div className="flex flex-col gap-4">
+      {data.map((file) => {
+        return (
+          <Card key={file._id}>
+            <Card.Header>
+              <Card.Title>{file._id}</Card.Title>
+            </Card.Header>
+            <Card.Content>
+              {file.tests.map((row) => {
+                const duration = !!row.endTime
+                  ? dayjs.duration(dayjs(row.endTime?.$date).diff(dayjs(row.startTime?.$date))).format("mm:ss")
+                  : null;
 
-      <Table.Body>
-        {data.map((row, index) => {
-          const duration = !!row.endTime ? dayjs(row.endTime).from(dayjs(row.startTime), true) : null;
+                return (
+                  <div key={row.testId} className="flex gap-2 items-center">
+                    <TestStatusIcon status={row.status} />
 
-          return (
-            <Table.Row key={index}>
-              <Table.Cell>{row.testId}</Table.Cell>
-              <Table.Cell>{row.title}</Table.Cell>
-              <Table.Cell>{duration}</Table.Cell>
-            </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
+                    <div>{row.title}</div>
+                    <div>{row.projectName}</div>
+                    <div>{duration}</div>
+                  </div>
+                );
+              })}
+            </Card.Content>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
+
+const TestStatusIcon = ({ status }: { status: TestResponse["status"] }) => {
+  switch (status) {
+    case "failed":
+      return (
+        <div>
+          <XCircle className="text-red-500 w-4 h-4" />
+        </div>
+      );
+
+    case "interrupted":
+      return <AlertCircle className="text-yellow-500 w-4 h-4" />;
+
+    case "passed":
+      return <CheckCircle2 className="text-green-500 w-4 h-4" />;
+
+    case "skipped":
+      return <SkipForward className="text-stone-500 w-4 h-4" />;
+
+    case "timedOut":
+      return <TimerOff className="text-stone-500 w-4 h-4" />;
+
+    default:
+      return null;
+  }
+};
