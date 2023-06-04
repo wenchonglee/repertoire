dfc5a3d8e13883c53b8f38f0f6d2806df94f8d3d
wenchong <18256786+wenchonglee@users.noreply.github.com>
2023-06-04 16:09:09 +0800

    fixed multiple prisma instance warning

--- packages/reporter/tests/failing-test.spec.ts
+++ packages/reporter/tests/failing-test.spec.ts
@@ -1,6 +1,7 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("has title 1", async ({ page }) => {
+  test.fail();
   await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
